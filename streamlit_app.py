7678cfcce85b669e4900a51d1ab5bf66af4c1050
Nidhiya Tittas <65657116+nidhiyatittas@users.noreply.github.com>
2024-04-30 14:51:17 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,8 @@ import numpy as np
 import pandas as pd
 import streamlit as st
 
-
+st.write("""
+# Multi sensor Synthetic data generation using Carla and Scenic """)
 genre = st.radio(
     "Pick a SCENIC behavior",
     ["Ego lane changing behavior", "Pedestrian walking through the sidewalk as ego passes behavior", "Ego applies breaks when pedestrian crosses the road behavior","Ego waiting for the pedestrian while taking reverse behavior","Pedestrian crossing before lane change behavior"])
