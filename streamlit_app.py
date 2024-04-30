b75e1db11e98306fd6efcf21f2a924967b70bd6c
Nidhiya Tittas <65657116+nidhiyatittas@users.noreply.github.com>
2024-04-30 14:56:40 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,6 +9,8 @@ genre = st.radio(
     "Pick a SCENIC behavior",
     ["Ego lane changing behavior", "Pedestrian walking through the sidewalk as ego passes behavior", "Ego applies breaks when pedestrian crosses the road behavior","Ego waiting for the pedestrian while taking reverse behavior","Pedestrian crossing before lane change behavior"])
 
+number = st.number_input("Enter the x value", value=None, placeholder="Type a number...")
+st.write('The x value is ', number)
 num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
 num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
 
