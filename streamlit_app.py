3a1cbfc613796e37cd7a18e7a97f998a534cc559
Nidhiya Tittas <65657116+nidhiyatittas@users.noreply.github.com>
2024-05-01 11:28:49 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,11 +6,6 @@ import streamlit as st
 st.write("""
 # Multi sensor Synthetic data generation using Carla and Scenic """)
 
-
-
-
-
-
 scenicBehavior = st.selectbox("Pick a SCENIC behavior", ["Ego lane changing behavior", "Pedestrian walking through the sidewalk as ego passes behavior", "Ego applies breaks when pedestrian crosses the road behavior","Ego waiting for the pedestrian while taking reverse behavior","Pedestrian crossing before lane change behavior","Ego Accelerating backward behaviour"])
 
 SensorNumber=st.selectbox("Number of sensors?", [1,2,3,4,5,6,7,8,9,10])
@@ -21,7 +16,7 @@ saveFlag = st.selectbox('Do you want to save generated data?', ["Yes","No"])
 sensor_locations = []
 for i in range(SensorNumber):
     sensor = st.radio(
-    "Pick a Sensor",
+    "Pick a Sensor {i+1}",
     ["Camera RGB","Camera Depth(Raw)"," Camera Depth (Gray Scale)"," Camera Depth (Logarithmic Gray Scale)","Camera Semantic Segmentation (Raw)","Camera Semantic Segmentation(CityScapes Palette)","Lidar (Ray-Cast)"],key=f"sensor_{i}")
     st.header(f"Location of Sensor {i+1}")
     xvalue = st.number_input("Enter the x value", value=None, placeholder="Type a number...", key=f"x_{i}")
