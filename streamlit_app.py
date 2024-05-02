6fd73484ee98ea0c4b41e8952ccf562490cd0400
Nidhiya Tittas <65657116+nidhiyatittas@users.noreply.github.com>
2024-05-02 15:30:29 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,6 +33,9 @@ sensor_mapping = {
 }
 col1, col2 = st.columns(2)
 with col1:  
+    st.write(" ")
+    t.write(" ")
+    t.write(" ")
     scenicBehavior = st.radio(
         "Pick a SCENIC behavior",
         list(behavior_mapping.keys()))  
