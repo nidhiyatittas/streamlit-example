fbfc49e8649b5d88a128246a7f78704c51a6b761
Nidhiya Tittas <65657116+nidhiyatittas@users.noreply.github.com>
2024-05-07 15:02:38 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -189,8 +189,8 @@ for i in range(SensorNumber):
        yawValue = st.number_input("Enter the Yaw value", value=0, placeholder="Type a number...", key=f"yaw_{i}")
 
     elif (view == "Right side view"):
-       xvalue = st.number_input("Enter the x value", value=6, placeholder="Type a number...", key=f"x_{i}")
-       yvalue = st.number_input("Enter the y value", value=0, placeholder="Type a number...", key=f"y_{i}") 
+       xvalue = st.number_input("Enter the x value", value=0, placeholder="Type a number...", key=f"x_{i}")
+       yvalue = st.number_input("Enter the y value", value=4, placeholder="Type a number...", key=f"y_{i}") 
        zvalue = st.number_input("Enter the z value", value=2.5, placeholder="Type a number...", key=f"z_{i}")
        st.header(f"Rotation values for the Sensor {i+1}")
        pitchValue = st.number_input("Enter the Pitch value", value=-15, placeholder="Type a number...", key=f"pitch_{i}")
